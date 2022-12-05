403bdb727ccc529097b0eecc22583b91caa4505f
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2022-12-05 11:17:53 +0530

    Update item.c

--- item.c
+++ item.c
@@ -9,7 +9,7 @@ struct prod
 
 int main()
 {
-    struct prod e[5];
+    struct prod e[5], temp;
     int i, n;
     for(i=0; i<5; i++)
     {
@@ -17,12 +17,12 @@ int main()
         fflush(stdin);
         scanf("%d", &e[i].id);
         fflush(stdin);
-        scanf("%s",&e[i].name);
+        scanf("%s",e[i].name);
         fflush(stdin);
         scanf("%f", &e[i].price);
     }
     
-    // diplay all items
+    // display all items
     for (i=0; i<5; i++)
     {
             printf("\n Item ID = %d", e[i].id);
@@ -45,7 +45,28 @@ int main()
             break;
         }
     }
+    
+    printf("\n------------------------------------------------");
+    printf("\nPrice Low to High\n\n");
+    for(i=0;i<5;i++){
+        for(int j=i+1;j<5;j++){
+            if(e[i].price>e[j].price){
+                temp = e[i];
+                e[i] = e[j];
+                e[j] = temp;
+                   
+            }
+        }
+    }
+    
+    for (i=0; i<5; i++)
+    {
+            printf("\n Item ID = %d", e[i].id);
+            printf("\n Name = %s", e[i].name);
+            printf("\n Price = %f", e[i].price);
+            printf("\n");
+    }
+    
+    
     return 0;
 }
-
-// made by piyush agarwal
