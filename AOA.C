b7b63fe38dd32cdf81b24ca9b6b3faa4cfcff6e8
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2022-12-05 17:04:24 +0530

    Create AOA.C

---
+++ AOA.C
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+struct prod
+{
+    int id;
+    char name[5];
+    float price;
+};
+
+    int main() {
+    int operation;
+
+
+    struct prod e[5], temp;
+    int i, n;
+    for(i=0; i<5; i++)
+    {
+        printf("\n Enter %d Item Id, Name and Price \n", i+1);
+        fflush(stdin);
+        scanf("%d", &e[i].id);
+        fflush(stdin);
+        scanf("%s",e[i].name);
+        fflush(stdin);
+        scanf("%f", &e[i].price);
+    }
+    
+    printf("\n choose an option (1) Display All Items, 2) Search an Item, 3) Sort: Cost low to high): ");
+    scanf("%d", &operation);
+    
+
+    switch(operation)
+    {
+        case 1:
+            for (i=0; i<5; i++)
+        {
+            printf("\n Item ID = %d", e[i].id);
+            printf("\n Name = %s", e[i].name);
+            printf("\n Price = %f", e[i].price);
+            printf("\n");
+        }
+        
+        printf("\n choose an option 2) Search an Item, 3) Sort: Cost low to high): ");
+        scanf("%d", &operation);
+
+        case 2:
+            printf("\n Enter Item Id to search for a product: \n");
+            scanf("%d", &n);
+    
+    
+            for(i=0; i<5; i++)
+            {
+                if(e[i].id==n)
+                {
+                    printf("\n Item ID = %d", e[i].id);
+                    printf("\n Name = %s", e[i].name);
+                    printf("\n Price = %f", e[i].price);
+                    break;
+                }
+            }
+            
+        printf("\n 3) Sort: Cost low to high): ");
+        scanf("%d", &operation);
+            
+        case 3:
+            printf("\nPrice Low to High\n\n");
+            
+            for(i=0;i<5;i++){
+                for(int j=i+1;j<5;j++){
+                    if(e[i].price>e[j].price){
+                        temp = e[i];
+                        e[i] = e[j];
+                        e[j] = temp;
+                           
+                    }
+                }
+            }
+        
+            for (i=0; i<5; i++)
+            {
+                    printf("\n Item ID = %d", e[i].id);
+                    printf("\n Name = %s", e[i].name);
+                    printf("\n Price = %f", e[i].price);
+                    printf("\n");
+            }
+                break;
+                
+        // operator doesn't match any case constant +, -, *, /
+        default:
+            printf("Error! Wrong option is choose by you");
+    }
+
+    return 0;
+}
