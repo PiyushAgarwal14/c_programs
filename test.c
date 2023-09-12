a307f9da9ac47b93753f8985f64680f3f32eb957
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2023-09-12 22:30:46 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    
+    int a[10] = {1,3,2,4,8,6,5};
+    int i, n;
+    
+    printf("Maximum value of array is: \n");
+    
+    for(i=0; i<=a[i]; i++) {
+       
+        if(a[i+1]>a[i]) {
+            n = a[i+1];
+        }
+    }
+    
+    printf("%d", n);
+    
+
+    
+    return 0;
+}
